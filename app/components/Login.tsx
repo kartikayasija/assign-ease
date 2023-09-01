5d9e0b43c832a57a55eb2665cd4a27cb0eaaee0e
Rolika Agarwal <rolikaagarwal2003@gmail.com>
2023-09-01 22:17:58 +0530

    login / signup with google added

---
+++ app/components/Login.tsx
@@ -0,0 +1,24 @@
+"use client";
+import { useSession, signIn, signOut } from "next-auth/react";
+import { Button } from "@/components/ui/button";
+
+const Login = () => {
+  const { data: session } = useSession();
+
+  if (session) {
+    return (
+      <div className="flex justify-end items-center mr-1">
+        <h5 className="mr-2">{session?.user?.name}</h5>
+        <Button onClick={() => signOut()}>Sign Out</Button>
+      </div>
+    );
+  }
+  return (
+    <div className="flex justify-end items-center mr-1">
+      <Button className="flex justify-end ml-auto" onClick={() => signIn()}>
+        Sign In
+      </Button>
+    </div>
+  );
+};
+export default Login;
