453363c2b6f746110a74ef6b6ae566245ed6afc7
Rolika Agarwal <rolikaagarwal2003@gmail.com>
2023-09-01 22:20:49 +0530

    login / sign up with google

--- app/components/Login.tsx
+++ app/components/Login.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { useSession, signIn, signOut } from "next-auth/react";
 import { Button } from "@/components/ui/button";
+import React from "react";
 
-const Login = () => {
+const Login : React.FC = () => {
   const { data: session } = useSession();
 
   if (session) {
